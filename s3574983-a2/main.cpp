f4ee918a781746172a5733f50a11cdc8e04b4b6f
Tuan Le <tlna95@icloud.com>
2019-04-30 19:02:14 +0700

    Initial Commit

---
+++ s3574983-a2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  s3574983-a2
+//
+//  Created by Tuan Le on 4/30/19.
+//  Copyright © 2019 RMIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
