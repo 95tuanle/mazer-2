0f9fbb08a75d52b84068b27560b2c397f3a49ada
Tuan Le <tlna95@icloud.com>
2023-05-02 03:13:08 -0400

    .gitignore is now working

--- Build/Intermediates.noindex/s3574983-a2.build/Debug/s3574983-a2.build/Objects-normal/x86_64/Edge.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/tuanle/Library/Mobile\ Documents/com~apple~CloudDocs/School/C++/s3574983-a2/s3574983-a2/Edge.cpp \
-  /Users/tuanle/Library/Mobile\ Documents/com~apple~CloudDocs/School/C++/s3574983-a2/s3574983-a2/Edge.hpp \
-  /Users/tuanle/Library/Mobile\ Documents/com~apple~CloudDocs/School/C++/s3574983-a2/s3574983-a2/Coordinator.hpp
--- Build/Intermediates.noindex/s3574983-a2.build/Debug/s3574983-a2.build/Objects-normal/x86_64/HuntAndKill.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/tuanle/Library/Mobile\ Documents/com~apple~CloudDocs/School/C++/s3574983-a2/s3574983-a2/HuntAndKill.cpp \
-  /Users/tuanle/Library/Mobile\ Documents/com~apple~CloudDocs/School/C++/s3574983-a2/s3574983-a2/HuntAndKill.hpp \
-  /Users/tuanle/Library/Mobile\ Documents/com~apple~CloudDocs/School/C++/s3574983-a2/s3574983-a2/Generator.hpp \
-  /Users/tuanle/Library/Mobile\ Documents/com~apple~CloudDocs/School/C++/s3574983-a2/s3574983-a2/Edge.hpp \
-  /Users/tuanle/Library/Mobile\ Documents/com~apple~CloudDocs/School/C++/s3574983-a2/s3574983-a2/Coordinator.hpp
